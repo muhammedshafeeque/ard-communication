8a207b042a2f803b9b8ffe1a4ca366dd908d6bab
Muhammed shafeeque P <shafeequkkv95@gmail.com>
2023-07-12 22:26:07 +0530

    dse modal updated

--- server/Models/DseModal.js
+++ server/Models/DseModal.js
@@ -2,12 +2,13 @@ import mongoose from "mongoose";
 import { collections } from "../Config/Collections.js";
 const dseModal = mongoose.Schema(
   {
-    mobile: { type: Number, required: true },
+    mobile: { type: Number, required: true,unique:true },
     stock: { type: Number, default: 0 },
-    activeDse: {
-      _id: { type: mongoose.Schema.ObjectId, required: true, ref: collections.PROFILE_COLLECTION },
+    activeUser: {
+      id: { type: mongoose.Schema.ObjectId, required: true, ref: collections.PROFILE_COLLECTION },
+      startsFrom:{type:Date}
     },
-    dses: [
+    userHistory: [
       {
         _id: { type: mongoose.Schema.ObjectId, required: true },
         startDate: { type: Date ,required:true},
