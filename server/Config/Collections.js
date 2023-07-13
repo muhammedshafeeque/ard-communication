32b8e1e8edfc4e4d4e47f4878bd22f15dc14196d
Muhammed shafeeque P <shafeequkkv95@gmail.com>
2023-07-13 22:26:19 +0530

    models compleated

--- server/Config/Collections.js
+++ server/Config/Collections.js
@@ -3,5 +3,11 @@ export const collections={
     PROFILE_COLLECTION:'profile',
     LINE_COLLECTION:'line',
     DSE_COLLECTION:'dse',
-    SHOPS_COLLECTION:'shop'
+    SHOPS_COLLECTION:'shop',
+    OUTSTANDINGS_COLLECTiON:'outstandings',
+    PAYMENT_COLLECTION:'payment',
+    DSE_STOCK_COLLECTION:'dse_stock',
+    SALES_CLOSS_COLLECTION:'sales_closs',
+    DSE_PAY_COLLECTION:'dse_pay'
+
 }
---
+++ server/Models/OutStandigsModal.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import { collections } from "../Config/Collections.js";
+const OutStandigsModel = mongoose.Schema(
+  {
+    shop: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: collections.SHOPS_COLLECTION,
+    },
+    line: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: collections.LINE_COLLECTION,
+    },
+    AddedUser: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: collections.PROFILE_COLLECTION,
+    },
+    amount: { type: Number, required: true },
+    billDate: { type: String, required: true },
+  },
+  {
+    timestaps: true,
+  }
+);
+export const OutStandigs = mongoose.model(
+  collections.OUTSTANDINGS_COLLECTiON,
+  OutStandigsModel
+);
