d5b1341384469c5a3c26cb0e1e73c2969de5ec4f
Muhammed shafeeque P <shafeequkkv95@gmail.com>
2023-07-14 18:06:32 +0530

    shop mapping compleated

--- server/Controllers/mappingsController.js
+++ server/Controllers/mappingsController.js
@@ -1,23 +1,25 @@
+import mongoose from "mongoose";
 import { Line } from "../Models/LineModal.js";
 import { Shope } from "../Models/ShopsModal.js";
 
 export const shopToLineMapping = async (req, res, next) => {
   try {
     let shopArray = [];
-    if (!req.body.length) next({ status: 400, message: "shpops are empty" });
+    if (!req.body.length)next({ status: 400, message: "shpops are empty" });
     let line = await Line.findById(req.params.id);
-    if (!line) next({ status: 400, message: "line Note Found" });
+    if (!line)next({ status: 400, message: "line Note Found" });
     await req.body.forEach(async (element) => {
       let shop = await Shope.findById(element);
-      if (shop) shopArray.push(element);
-      next({ status: 400, message: "some Shops note fount" })
-    })
-  
-      await Line.findByIdAndUpdate(req.params.id, {
-        $push: { shops: { $each: req.body } },
-      });
-      res.send("Shops mapped Successfully");
-
+      if (shop) shopArray.push(element)
+      else next({ status: 400, message: "some Shops note fount" });
+    });
+    await req.body.forEach((str) => {
+      shopArray.push(new mongoose.Types.ObjectId(str));
+    });
+    await Line.findByIdAndUpdate(req.params.id, {
+      $addToSet: { shops: { $each: shopArray } },
+    });
+    res.send("Shops mapped Successfully");
   } catch (error) {
     next(error);
   }
