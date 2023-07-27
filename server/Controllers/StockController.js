758bfca022ec5bc61e46d844f4039033e76c3f7c
Muhammed shafeeque P <shafeequkkv95@gmail.com>
2023-07-27 11:55:19 +0530

    updated

--- server/Controllers/StockController.js
+++ server/Controllers/StockController.js
@@ -3,20 +3,35 @@ import { DSE_STOCK } from "../Models/DseStockModal.js";
 
 export const addStock = async (req, res, next) => {
   try {
-    let payload={
-        dse:req.body.dseId,
-        amount:req.body.amount
-
+    let payload = {
+      dse: req.body.dseId,
+      amount: req.body.amount,
+    };
+    let dse = await DSE.findById(req.body.dseId);
+    if (!dse) {
+      next({ status: 400, message: "Dse Not Fount" });
+    } else {
+      await DSE_STOCK.create(payload);
+      await DSE.findByIdAndUpdate(req.body.dseId, {
+        $inc: {
+          stock: req.body.amount,
+        },
+      });
+      res.send("stock Added Successfully");
     }
-    await DSE_STOCK.create(payload);
-    await DSE.findByIdAndUpdate(req.body.dseId, {
-      $inc: {
-        stock:req.body.amount,
-      },
-    });
-    res.send("stock Added Successfully");
   } catch (error) {
-    console.log(error)
+    next(error);
+  }
+};
+export const getStocks = async (req, res, next) => {
+  let query=req.query
+  try {
+    let stocks = await DSE_STOCK.find()
+      .populate("dse")
+      .limit(query.limit ? parseInt(query.limit) : 10)
+      .skip(query.offset ? parseInt(query.offset) : 0);
+    res.send(stocks);
+  } catch (error) {
     next(error);
   }
 };
--- server/Router/StockRouter.js
+++ server/Router/StockRouter.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import { Validate } from '../MiddleWare/Validation.js'
 import { stockValidation } from '../Validations/stockValidations.js'
-import { addStock } from '../Controllers/StockController.js'
+import { addStock, getStocks } from '../Controllers/StockController.js'
 const router=express.Router()
 router.post('/stock',Validate(stockValidation),addStock)
+router.get('/stock',getStocks)
 export const stockRouter=router
