ab7aedb30f804ba3ae3759f8df38344551e7c200
anwarkvzk <anvarvzk521@gmail.com>
2023-07-27 08:26:22 +0530

    Conflict removed

--- ard/src/Constants/constant.jsx
+++ ard/src/Constants/constant.jsx
@@ -1,11 +1,7 @@
 export const TOKEN='token'
-<<<<<<< HEAD
-// export const BASE_URL='http://52.23.184.175:5000/api/'
-//   export const BASE_URL='http://localhost:5000/api/'
-=======
+
  export const BASE_URL='http://52.23.184.175:5000/api/'
  // export const BASE_URL='http://localhost:5000/api/'
->>>>>>> main
  export const mappingTypes=[
     {name:'User-Dse Mapping', code : 'UDM'},
     {name:'Line-Dse Mapping', code : 'LDM'},
